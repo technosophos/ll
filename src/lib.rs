da1e63a77056a74b538b8d7abf9ca3ad3c5696ae
Matt Butcher <matt.butcher@microsoft.com>
2019-12-20 19:48:39 -0700

    Updated icons
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,59 @@
 #[macro_use]
 extern crate maplit;
 
+use chrono::prelude::*;
+use colorful::{Color, Colorful};
 use maplit::hashmap;
+use size_format::SizeFormatterSI;
 use std::collections::HashMap;
 use std::env::args;
 use std::ffi::{OsStr, OsString};
 use std::fs::read_dir;
+use std::time::UNIX_EPOCH;
+
+pub struct Entry {
+    pub icon: String,
+    pub name: String,
+    pub bytes: u64,
+    pub mode: u32,
+    pub modified: std::time::SystemTime,
+    pub executable: bool,
+    pub dir: bool,
+}
+
+impl Entry {
+    /// Read bytes and convert to a pretty size.
+    pub fn pretty_size(&self) -> String {
+        format!("{}B", SizeFormatterSI::new(self.bytes))
+    }
+    pub fn pretty_time(&self) -> String {
+        Local
+            .timestamp_millis(
+                self.modified
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap_or_else(|_| std::time::Duration::new(0, 0))
+                    .as_millis() as i64,
+            )
+            .format("   %D    %R")
+            .to_string()
+    }
+    pub fn pretty_icon(&self) -> String {
+        if self.executable {
+            let ico = if self.icon == DEFAULT_ICON {
+                "\u{fc0c}".to_string() // Running man
+            } else {
+                self.icon.clone()
+            };
+            return ico.color(Color::Red1).to_string();
+        }
+        self.icon.clone()
+    }
+}
 
 pub struct IconResolver {
     file_icons: HashMap<String, String>,
     ext_icons: HashMap<String, String>,
+    dir_icons: HashMap<String, String>,
 }
 
 const DEFAULT_ICON: &str = "\u{f036}";
@@ -17,29 +61,23 @@ const FOLDER_ICON: &str = "\u{f07b}";
 const LINK_ICON: &str = "\u{f481}";
 
 impl IconResolver {
-    pub fn new() -> Self {
-        let file_icons = hashmap![
-            ".Dockerfile".into() => "\u{e7b0}".into(),
-        ];
-        let ext_icons = hashmap![
-            "rs".into() => "\u{e7a8}".into(),
-        ];
-        IconResolver {
-            file_icons,
-            ext_icons,
-        }
-    }
     pub fn icon(&self, entry: &std::fs::DirEntry) -> String {
-        if let Ok(ft) = entry.file_type() {
-            if ft.is_dir() {
-                return FOLDER_ICON.to_string();
-            }
-            if ft.is_symlink() {
-                return LINK_ICON.to_string();
-            }
-        }
         let file_name = entry.file_name();
         let n = file_name.to_str().unwrap();
+        if let Ok(ft) = entry.file_type() {
+            if ft.is_dir() {
+                return self
+                    .dir_icons
+                    .get(n)
+                    .map(|i| i.to_string())
+                    .unwrap_or_else(|| FOLDER_ICON.to_string())
+                    .color(Color::Yellow)
+                    .to_string();
+            }
+            if ft.is_symlink() {
+                return LINK_ICON.to_string().color(Color::Aquamarine1a).to_string();
+            }
+        }
         match self.file_icons.get(n) {
             Some(icon) => icon.to_string(),
             None => {
@@ -55,6 +93,94 @@ impl IconResolver {
                     .map(|i| i.to_string())
                     .unwrap_or_else(|| DEFAULT_ICON.to_string())
             }
+        }
+    }
+}
+
+/// Build a map icons mapped to file extensions or file names.
+macro_rules! icomap {
+    (
+        $(
+            $icon:expr => [ $($i: expr),* ]
+        ),*
+    ) => {
+        {
+            let mut icons: HashMap<String, String> = HashMap::new();
+            $(
+                $(
+                    icons.insert($i.to_string(), $icon.to_string());
+                )*
+            )*
+            icons
+        }
+    };
+}
+
+impl Default for IconResolver {
+    fn default() -> Self {
+        let file_icons = icomap![
+            "\u{f113}" => [".gitignore"],
+            "\u{e20f}" => ["Makefile", "justfile"],
+            "\u{e7b0}" => ["Dockerfile"],
+            "\u{f0c3}" => ["brigade.js"],
+            "\u{f1d8}" => ["glide.yaml", "Gopkg.toml"],
+            "\u{f6f6}" => ["go.mod", "go.sum"],
+            "\u{e718}" => ["package.json"],
+            "\u{e711}" => [".DS_Store"],
+            "\u{f071}" => ["LICENSE", "COPYING", "LICENSE", "license.txt", "LICENSE.txt", "COPYING.txt"],
+            "\u{f1b2}" => ["Cargo.toml"],
+            "\u{f120}" => [".bashrc", ".zshrc", ".profile"]
+        ];
+
+        let ext_icons = icomap![
+            "\u{e7a8}" => ["rs"],
+            "\u{f023}" => ["lock"],
+            "\u{f120}" => [".sh", ".bash", ".zsh", ".ksh"],
+            "\u{e781}" => ["js"],
+            "\u{f18d}" => ["log"],
+            "\u{f19e}" => ["yaml", "yml"],
+            "\u{e60b}" => ["json"],
+            "\u{f085}" => ["conf", "config", "cfg"],
+            "\u{f1b3}" => ["bin", "exe"],
+            "\u{f1e2}" => ["err"],
+            "\u{e628}" => ["ts"],
+            "\u{e73c}" => ["py", "pyc", "pyo"],
+            "\u{e609}" => ["md", "markdown"],
+            "\u{e739}" => ["rb"],
+            "\u{e738}" => ["java", "class"],
+            "\u{e626}" => ["go"],
+            "\u{f1c9}" => ["html", "htm", "xhtml"],
+            "\u{e73d}" => ["php", "phar"],
+            "\u{e60e}" => ["tpl", "tmpl"],
+            "\u{e61b}" => [".coffee"],
+            "\u{f1c6}" => ["tgz", "gz", "zip", "bz2", "bz"],
+            "\u{f1c1}" => ["pdf"],
+            "\u{e60d}" => ["png", "gif", "jpg", "jpeg", "ico", "svg", "tiff", "bmp"],
+            "\u{f1c2}" => ["doc", "docx", "odt"],
+            "\u{f1c4}" => ["ppt", "pptx"],
+            "\u{f1c3}" => ["xls", "xlsx"],
+            "\u{f0ce}" => ["csv", "tsv", "tab"],
+            "\u{f084}" => ["pgp", "gpg", "gpg~", "kbx", "kbx~"],
+            "\u{f1c0}" => ["db", "sqlite", "sqlite3"],
+            "\u{f008}" => ["mov", "mp4"],
+            "\u{f0fa}" => ["backup", "old", "OLD"],
+            "\u{e7c5}" => ["vim"],
+            "\u{e20f}" => ["mk", "make"],
+            "\u{f0fd}" => ["h"],
+            "\u{e61e}" => ["c"],
+            "\u{e61d}" => ["cc", "c++"],
+            "\u{f783}" => ["toml", "tml"],
+            "\u{f0f6}" => ["txt"]
+        ];
+
+        let dir_icons = icomap![
+            "\u{e5fb}" => [".git"]
+        ];
+
+        IconResolver {
+            file_icons,
+            ext_icons,
+            dir_icons,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,67 @@
+use clap::{App, Arg};
+use ll::*;
 use std::env::args;
-use std::ffi::{OsStr, OsString};
+use std::os::unix::fs::PermissionsExt;
+//use std::ffi::{OsStr, OsString};
+
 use std::fs::read_dir;
 
-const FILENAME_WIDTH: i8 = 30;
-const MAX_WIDTH: i8 = 80;
+const FILENAME_WIDTH: usize = 80;
+//const MAX_WIDTH: i8 = 80;
 
 type LLResult = Result<(), failure::Error>;
 
 fn main() -> LLResult {
-    let dir = args().nth(1).unwrap_or_else(|| "./".into());
-    print_entries(dir)
+    let matches = App::new("A pretty version of 'ls'")
+        .version("2.0.0")
+        .author("Technosophos")
+        .about("List the contents of a directory, with pretty icons and human-readable sizes and dates.")
+        .arg(Arg::with_name("all").short("a").long("all").help("View all files, including hidden ones").takes_value(false))
+        .arg(Arg::with_name("DIR").help("The directory to view").index(1))
+        .get_matches();
+    let dir = matches.value_of("DIR").unwrap_or("./").to_string();
+    let all = matches.is_present("all");
+    print_entries(dir, all)
 }
 
-fn print_header() {}
-fn print_footer() {}
-fn print_entries(dirname: String) -> LLResult {
-    for fi in read_dir(dirname)? {
-        let icons = ll::IconResolver::new();
-        let entry = fi?;
+//fn print_header() {}
+//fn print_footer() {}
+fn print_entries(dirname: String, show_hidden: bool) -> LLResult {
+    let mut dirs: Vec<_> = read_dir(dirname)?.map(|r| r.unwrap()).collect();
+    dirs.sort_by_key(|d| d.path());
+    let icons = IconResolver::default();
+    let mut width = 0;
+    let mut lines = vec![];
+    for entry in dirs {
         let file_name = entry.file_name();
         let fname = file_name.to_str().unwrap();
+        if !show_hidden && fname.starts_with('.') {
+            continue;
+        }
+        width = fname.len().max(width).min(FILENAME_WIDTH);
         let icon = icons.icon(&entry);
-        println!("{}  {}", icon, fname)
+        let mode = entry.metadata()?.permissions().mode();
+        lines.push(Entry {
+            icon,
+            name: fname.to_string(),
+            bytes: entry.metadata()?.len(),
+            modified: entry.metadata()?.modified()?,
+            executable: mode & 0o100 != 0,
+            mode: entry.metadata()?.permissions().mode(),
+            dir: entry.file_type()?.is_dir(),
+        });
+        println!("Mode shifted {}: {}", fname, mode & 0o100);
+    }
+    for l in lines {
+        println!(
+            "{}  {: <width$}  {:>8} {} {:>6o}",
+            l.pretty_icon(),
+            l.name,
+            l.pretty_size(),
+            l.pretty_time(),
+            l.mode,
+            width = width
+        );
     }
     Ok(())
 }
